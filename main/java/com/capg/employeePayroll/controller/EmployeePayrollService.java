e0341b393cdbc1b8273c854a232bdce90f6fc7ec
Jay002 <jayesh2598@gmail.com>
2020-10-18 20:08:30 +0530

    Added: Employee payroll service to read from and write to console

---
+++ main/java/com/capg/employeePayroll/controller/EmployeePayrollService.java
@@ -0,0 +1,45 @@
+package com.capg.employeePayroll.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.capg.employeePayroll.model.EmployeePayrollData;
+
+public class EmployeePayrollService {
+
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+	}
+
+	private List<EmployeePayrollData> employeePayrollList;
+
+	public EmployeePayrollService() {
+	}
+
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+		Scanner consoleInputReader = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData();
+	}
+
+	private void readEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("Enter the employee ID : ");
+		int id = consoleInputReader.nextInt();
+		System.out.println("Enter the employee name : ");
+		String name = consoleInputReader.next();
+		System.out.println("Enter the employee's salary : ");
+		double salary = consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+	}
+
+	private void writeEmployeePayrollData() {
+		System.out.println("Writing Employee payroll data on Console: " + employeePayrollList);
+	}
+}
